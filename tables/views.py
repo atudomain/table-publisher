5fee5bbbd5501891cd3096cdf57df06bcd56ce33
Adrian Tuzimek <tuziomek@gmail.com>
2020-08-02 14:12:12 +0200

    Implemented GET and DELETE table by name

--- tables/views.py
+++ tables/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import generic
+from django.shortcuts import get_object_or_404
+
 from .models import Table, Row, Column
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework import status
+from rest_framework.response import Response
 from .serializers import TableSerializer, RowSerializer, ColumnSerializer
 
 
@@ -25,6 +29,17 @@ class TableViewSet(viewsets.ModelViewSet):
     queryset = Table.objects.all()
     serializer_class = TableSerializer
 
+    def retrieve(self, request, pk=None):
+        queryset = Table.objects.all()
+        table = get_object_or_404(queryset, name=pk)
+        serializer = TableSerializer(table)
+        return Response(serializer.data)
+
+    def destroy(self, request, pk=None):
+        queryset = Table.objects.all()
+        table = get_object_or_404(queryset, name=pk)
+        table.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class RowViewSet(viewsets.ModelViewSet):
     """
